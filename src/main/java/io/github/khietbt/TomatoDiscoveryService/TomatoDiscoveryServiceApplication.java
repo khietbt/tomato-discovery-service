1c07ab9b258b179a2e0fcf9a8a5061ed4b8a4dd6
khietbt <khietbt@clevai.edu.vn>
2023-01-31 00:24:02 +0700

    Initial commit

---
+++ src/main/java/io/github/khietbt/TomatoDiscoveryService/TomatoDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.khietbt.TomatoDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class TomatoDiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TomatoDiscoveryServiceApplication.class, args);
+    }
+}
