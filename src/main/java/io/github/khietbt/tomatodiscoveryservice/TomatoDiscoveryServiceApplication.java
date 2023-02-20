2a540e8ea8938807ddc0e2e9d33072100b422df2
khietbt <khietbt@clevai.edu.vn>
2023-02-20 14:02:10 +0700

    Correct package name to avoid sonar's complains

--- src/main/java/io/github/khietbt/tomatodiscoveryservice/TomatoDiscoveryServiceApplication.java
+++ src/main/java/io/github/khietbt/tomatodiscoveryservice/TomatoDiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package io.github.khietbt.TomatoDiscoveryService;
+package io.github.khietbt.tomatodiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
